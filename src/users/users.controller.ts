f5de1464c05ddd0bd3975435de5f9a6e177f1849
asma <asma.ym@mail.kmutt.ac.th>
2023-12-10 11:24:43 +0700

    add register endpoint for testing

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -28,6 +28,9 @@ export class UsersController {
     }
   }
 
+  @Post('register')
+  register() {}
+
   @Get(':id') // Get /users/id
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
